c86f10c4aa1361898c3d2a9518c4d05e3f619bf7
LiamHao <smile176223@gmail.com>
2022-07-13 09:11:20 +0800

    question1 save and fetch function

--- iOS-Interview/Storers/CoreData/CoreDataStorer.swift
+++ iOS-Interview/Storers/CoreData/CoreDataStorer.swift
@@ -27,7 +27,13 @@ extension CoreDataStorer {
     // Step 1. Convert User to UserTable
     // Step 2. Save UserTable to CoreDataService (with .store(UserTable))
     func save(user: User) {
+        let userTable = UserTable(id: user.id,
+                                  field_name: user.name,
+                                  field_email: user.email,
+                                  field_is_designer: user.isDesigner,
+                                  created_at: Date())
         
+        coreDateService.store(userTable)
     }
     
     // TODO:
@@ -35,6 +41,20 @@ extension CoreDataStorer {
     // Step 1-1. if User.id is not exists in coreDateService than completion with notFound Failure
     // Step 1-2. if User.id is exists in coreDateService than convert it to User and completion with User Success
     func fetch(user: User, completion: @escaping (Result<User, CoreDataStorerError>) -> Void) {
-
+        
+        coreDateService.fetchAll(UserTable.self) { coreDataTables in
+            for coreDataTable in coreDataTables {
+                if coreDataTable.id == user.id {
+                    let user = User(id: coreDataTable.id,
+                                    name: coreDataTable.field_name,
+                                    email: coreDataTable.field_email,
+                                    isDesigner: coreDataTable.field_is_designer)
+                    completion(.success(user))
+                    break
+                }
+            }
+            completion(.failure(CoreDataStorerError.notFound))
+        }
+        
     }
 }
